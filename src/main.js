81d497eeb3b31803a7fea2cd87a5f27756afbf51
Mehmet Uysal <mehmetuysal2000@yandex.com>
2022-04-12 18:59:35 +0300

    0.0.1

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import style from "@/style/style.css";
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(router).use(style).mount("#app");
+createApp(App).use(Quasar, quasarUserOptions).use(router).use(style).mount("#app");
